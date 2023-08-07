30b92bf5bc5b507b4be7bfa2dc8dc025327a3ff8
karan rajput <121344093+KaransingRajput@users.noreply.github.com>
2023-08-07 15:01:43 +0530

    initial commit

---
+++ src/main/java/WorkWithHibernate/App.java
@@ -0,0 +1,17 @@
+package WorkWithHibernate;
+
+import org.hibernate.cfg.configuration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        Configuration cfg=new Configuration();
+        
+    }
+}
---
+++ src/main/java/WorkWithHibernate/Student.java
@@ -0,0 +1,9 @@
+package WorkWithHibernate;
+@Entity
+@Table(name="StudentDetails")
+public class Student {
+private int id;
+private String name;
+private String city;
+
+}
